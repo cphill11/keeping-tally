00b741d388e2f7bf20f6b7c84da20eff87a336b6
Criste Phillips <criste.phillips@yahoo.com>
2023-07-31 11:25:41 -0400

    initial javascript and jquery for calendar functionality in place

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,10 +1,61 @@
 // get id est for current date & time
-const currentDate = document.getElementById("currentDate");
+var currentDate = document.getElementById("currentDate");
     // display current date & time using moment.js 
     currentDate.textContent = moment().format('MMM Do YYYY, h:mm:ss a');
 
 // calendar row color functionality
 function colorChange() {
-    let hour = moment().hour();
-    
+   var hour = moment().hour();
+
+    // jquery variable used for each time-block class
+    $(".time-block").each(function() {
+        var rowHour = parseInt($(this).attr("id").split("-")[1]);
+
+        // evaluate rowHour, apply color based on past, prestent, or future class;
+        if (rowHour < hour) {
+            $(this).addClass("past");     // row will be gray
+        } else if (rowHour === hour) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+        } else {
+            $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+        }
+
+    });
 }
+colorChange();
+
+// save key / value to to local Storage
+$(".saveBtn").on("click", function () {
+    let saveKey = $(this).parent().attr("id");
+    let saveValue = $(this).siblings(".description").val();
+    localStorage.setItem(saveKey, saveValue);
+});
+
+//pull row info from localStorage & display it in the description
+$("#row-5 .description").val(localStorage.getItem("row-5)"));
+$("#row-6 .description").val(localStorage.getItem("row-6)"));
+$("#row-7 .description").val(localStorage.getItem("row-7)"));
+$("#row-8 .description").val(localStorage.getItem("row-8)"));
+$("#row-9 .description").val(localStorage.getItem("row-9)"));
+$("#row-11 .description").val(localStorage.getItem("row-11)"));
+$("#row-12 .description").val(localStorage.getItem("row-12)"));
+$("#row-13 .description").val(localStorage.getItem("row-13)"));
+$("#row-14 .description").val(localStorage.getItem("row-14)"));
+$("#row-15 .description").val(localStorage.getItem("row-15)"));
+$("#row-16 .description").val(localStorage.getItem("row-16)"));
+$("#row-17 .description").val(localStorage.getItem("row-17)"));
+$("#row-18 .description").val(localStorage.getItem("row-18)"));
+$("#row-19 .description").val(localStorage.getItem("row-19)"));
+$("#row-20 .description").val(localStorage.getItem("row-20)"));
+$("#row-21 .description").val(localStorage.getItem("row-21)"));
+$("#row-22 .description").val(localStorage.getItem("row-22)"));
+$("#row-23 .description").val(localStorage.getItem("row-23)"));
+$("#row-24 .description").val(localStorage.getItem("row-24)"));
+$("#row-25 .description").val(localStorage.getItem("row-25)"));
+$("#row-26 .description").val(localStorage.getItem("row-26)"));
+$("#row-27 .description").val(localStorage.getItem("row-27)"));
+$("#row-28 .description").val(localStorage.getItem("row-28)"));
